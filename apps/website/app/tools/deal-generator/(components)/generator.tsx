252c40f71c2082d4e849a3a3659c1306bcbed3af
Aaron Hutton <aaron@the-huttons.co.uk>
2023-06-29 22:12:10 +0100

    Fixing website after generator api changes

--- apps/website/app/tools/deal-generator/(components)/generator.tsx
+++ apps/website/app/tools/deal-generator/(components)/generator.tsx
@@ -4,24 +4,24 @@ import { DealViewer } from "@/components/bridge/deal-viewer";
 import { Button } from "@/components/ui/button";
 import { EMPTY_DEAL } from "@/utils/constants";
 import { Types } from "@bridge-tools/core";
-import { generateDeals } from "@bridge-tools/generator";
+import { generate } from "@bridge-tools/generator";
 import { useEffect, useState } from "react";
 
 export function Generator() {
   const [deal, setDeal] = useState<Types.Deal>(EMPTY_DEAL);
 
-  const generate = () => {
-    const deal = generateDeals({})[0];
+  const generateDeal = () => {
+    const deal = generate({})[0];
     setDeal(deal);
   };
 
   useEffect(() => {
-    generate();
+    generateDeal();
   }, []);
 
   return (
     <div className="flex w-full flex-col items-center space-y-8">
-      <Button onClick={generate}>Generate Deal</Button>
+      <Button onClick={generateDeal}>Generate Deal</Button>
       <DealViewer deal={deal} />
     </div>
   );
